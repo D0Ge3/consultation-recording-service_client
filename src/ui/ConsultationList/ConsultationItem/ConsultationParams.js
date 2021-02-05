b5587ecec2b70a99e7b6ec5c3c8d7fa2a9d4eeb0
d0ge3 <fnik340@gmail.com>
2021-02-05 18:18:29 +0300

    fix: view icons w/out contente (ex. note icon views w/out note in ConsultationParams)

--- src/ui/ConsultationList/ConsultationItem/ConsultationParams.js
+++ src/ui/ConsultationList/ConsultationItem/ConsultationParams.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react'
 
-import { GeoAlt, InfoCircle, JournalText, PersonFill } from 'react-bootstrap-icons'
+import {
+  GeoAlt,
+  InfoCircle,
+  JournalText,
+  PersonFill,
+} from 'react-bootstrap-icons'
 
 import s from './ConsultationItem.module.css'
 
-export const ConsultationParams = ({ subjects, note, location, type, role, consultation_type, link }) => {
+export const ConsultationParams = ({
+  subjects,
+  note,
+  location,
+  type,
+  role,
+  consultation_type,
+  link,
+}) => {
   let [showNote, setShowNote] = useState(false)
   const paramStyle = type === 'widget' ? { width: '300px' } : {}
   const subjectsList = subjects.map((s) => (
@@ -15,41 +28,46 @@ export const ConsultationParams = ({ subjects, note, location, type, role, consu
   return (
     <div className={s.params} style={paramStyle}>
       {(type === 'widget' && role === 'student') ||
-        (type === 'schedule' || type === 'info') && (
+        ((type === 'schedule' || type === 'info') && (
           <div className={s.teacherWrapper}>
-            <PersonFill className={s.teacherIcon}/>
+            <PersonFill className={s.teacherIcon} />
             <span className={s.teacherName}>{teacher_fio}</span>
           </div>
-        )}
+        ))}
       <div className={s.subjectListWrapper}>
         <JournalText className={s.subjectIcon} />
         {subjectsList}
       </div>
       {note.length > 50 && !showNote ? (
-        <div className={s.noteWrapper}><InfoCircle className={s.noteIcon} />
+        <div className={s.noteWrapper}>
+          <InfoCircle className={s.noteIcon} />
           <span className={s.noteBtn} onClick={() => setShowNote(true)}>
             Показать примечание...
           </span>
         </div>
       ) : (
-        <div className={s.noteWrapper}>
-          <InfoCircle className={s.noteIcon} />
-          <span className={s.note}>{note}</span>
+        note && (
+          <div className={s.noteWrapper}>
+            <InfoCircle className={s.noteIcon} />
+            <span className={s.note}>{note}</span>
+          </div>
+        )
+      )}
+      {(location || link) && (
+        <div className={s.location}>
+          <GeoAlt className={s.locationIcon} />
+          {consultation_type === 'Очная' ? (
+            <span>{location}</span>
+          ) : (
+            <a
+              href={link}
+              title={!link ? 'Ссылка будет доступна после записи' : ''}
+            >
+              Дистанционно
+            </a>
+          )}
         </div>
       )}
-      <div className={s.location}>
-        <GeoAlt className={s.locationIcon} />
-        {consultation_type === 'Очная' ? (
-          <span>{location}</span>
-        ) : (
-          <a
-            href={link}
-            title={!link ? 'Ссылка будет доступна после записи' : ''}
-          >
-            Дистанционно
-          </a>
-        )}
-      </div>
     </div>
   )
 }
